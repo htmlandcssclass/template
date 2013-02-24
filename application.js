52dd7e4ded3ac16f6c3be9863c4c0eda94425183
OpenShift guest <seanjonphenom@gmail.com>
2013-02-24 14:43:28 -0500

    Added bunch more properties.

--- application.js
+++ application.js
@@ -1,13 +1,41 @@
+var PropertyControl = window.PropertyControl;
+
 $(function(){
-    var paddingEl = $('[name=padding]');
-    var marginEl = $('[name=margin]');
-    var liEls = $('li');
+//    var displayEl = $('#second-paragraph');
+    var displayEl = $('p');
+    new PropertyControl('width', $('#width-control'), displayEl, {
+        initialValue: 400
+    });
+    new PropertyControl('height', $('#height-control'), displayEl, {
+        initialValue: 200
+    });
+    new PropertyControl('padding', $('#padding-control'), displayEl, {
+        initialValue: 15,
+        disabledValue: 0
+    });
+    new PropertyControl('margin', $('#margin-control'), displayEl, {
+        initialValue: 15,
+        disabledValue: 0
+    });
     
-    paddingEl.change(function(){ setStyle(paddingEl, 'padding'); });
-    marginEl.change(function(){ setStyle(marginEl, 'margin'); });
+    new PropertyControl('border', $('#border-control'), displayEl, {
+        initialValue: '1px solid #ccc',
+        disabledValue: 'none'
+    });
+    new PropertyControl('background-color', $('#background-color-control'), displayEl, {
+        initialValue: '#eee',
+        disabledValue: 'transparent'
+    });
     
-    var setStyle = function(control, property){
-        var value = control.val();
-        liEls.css(property, value);
-    };
+    new PropertyControl('text-decoration', $('#text-decoration-control'), displayEl, {
+        initialValue: 'underline',
+        disabledValue: 'none'
+    });
+    new PropertyControl('font-weight', $('#font-weight-control'), displayEl, {
+        initialValue: 'bold',
+        disabledValue: 'normal'
+    });
+    new PropertyControl('line-height', $('#line-height-control'), displayEl, {
+        initialValue: 16
+    });
 });
---
+++ property_control.js
@@ -0,0 +1,41 @@
+var PropertyControl = function(property, controlEl, displayEl, _options){
+    _options = _options ? _options : {};
+    var options = {
+        initialValue: _options.initialValue ? _options.initialValue : 0,
+        disabledValue: _options.disabledValue ? _options.disabledValue : 'initial'
+    }
+    
+    var enabledInput = controlEl.find('[name='+ property +'-enabled]');
+    var valueInput = controlEl.find('[name='+ property +'-value]');
+    var valueOutput = controlEl.find('.value');
+    var value = options.initialValue;
+
+    var showValue = function(value){
+        valueOutput.text(formattedValue(value));
+        displayEl.css(property, formattedValue(value));
+    };
+    
+    var formattedValue = function(value){
+        var isANumber = value > 0 || value < 0;
+        return isANumber ? value+'px' : value;
+    };
+    
+    var enableOrDisable = function(){
+        var disabled = !enabledInput.is(':checked');
+        controlEl.toggleClass('disabled', disabled);
+        valueInput.prop('disabled', disabled);
+        if (disabled){
+            showValue(options.disabledValue);
+        } else {
+            showValue(value);
+        }
+        
+    };
+    
+    enabledInput.on('change', enableOrDisable);
+    valueInput.on('change', function(){
+        value = valueInput.val();
+        showValue(value);
+    });
+    enableOrDisable();
+};
